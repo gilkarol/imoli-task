2ccb54d8a71c2d52a3accb893cb817671d5ce954
Karol <gilkarol304@gmail.com>
2022-02-14 19:31:15 +0100

    added getFilms route

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,5 +1,26 @@
-import { Router } from 'express'
+import { Response, Request, NextFunction, Router } from 'express'
+
+import fetch from 'node-fetch'
 
 const router = Router()
+
+router.get(
+	'/films',
+	async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const response = await (await fetch('https://swapi.dev/api/films/')).json()
+			const result = response.results.map((item: any) => {
+				return {
+					release_date: item.release_date,
+					title: item.title,
+					episode_id: item.episode_id,
+				}
+			})
+			res.status(200).json({ films: result })
+		} catch (err) {
+			console.log(err)
+		}
+	}
+)
 
 export default router
