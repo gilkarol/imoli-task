6b89c0c308c62dee6970046380f2f1c41d1af9d7
Karol <gilkarol304@gmail.com>
2022-02-14 19:58:05 +0100

    added mongoose connect

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express from 'express'
 import routes from './routes/main'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config({ path: './.env' })
 
 import bodyparser from 'body-parser'
 
@@ -8,4 +11,11 @@ const app = express()
 app.use(bodyparser.json())
 app.use(routes)
 
-app.listen(8080)
+mongoose
+	.connect(process.env.MONGODB_DATABASE as string)
+	.then((result) => {
+		app.listen(8080)
+	})
+	.catch((err) => {
+		console.log(err)
+	})
