57b09968c37a81c7ac96b70902c9210d6db46a53
Karol <gilkarol304@gmail.com>
2022-02-15 12:45:15 +0100

    added dist

---
+++ dist/app.js
@@ -0,0 +1,22 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const main_1 = __importDefault(require("./routes/main"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config({ path: './.env' });
+const body_parser_1 = __importDefault(require("body-parser"));
+const app = (0, express_1.default)();
+app.use(body_parser_1.default.json());
+app.use(main_1.default);
+mongoose_1.default
+    .connect(process.env.MONGODB_DATABASE)
+    .then((result) => {
+    app.listen(8080);
+})
+    .catch((err) => {
+    console.log(err);
+});
