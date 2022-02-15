3ef03fa6a80ae6c6a46a37e5463a432574b86091
Karol <gilkarol304@gmail.com>
2022-02-15 13:17:40 +0100

    added cors middleware

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { Response, Request, NextFunction } from 'express'
 import routes from './routes/main'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
@@ -7,8 +7,22 @@ dotenv.config({ path: './.env' })
 import bodyparser from 'body-parser'
 
 const app = express()
+
 app.use(bodyparser.json())
+
+app.use((req: Request, res: Response, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader(
+	  'Access-Control-Allow-Methods',
+	  'GET, POST'
+	);
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+	next();
+  });
+
 app.use(routes)
+
+
 mongoose
 	.connect(process.env.MONGODB_DATABASE as string)
 	.then((result) => {
